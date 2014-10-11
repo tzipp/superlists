7ffe5594dff3be6561b069f8887a7e32ec0d9e7f
VZip <vinzipped@gmail.com>
2014-10-11 18:44:15 -0400

    Hack FT runner to be able to test staging. You silly goat!

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,8 +1,25 @@
+import sys
 from django.contrib.staticfiles.testing  import StaticLiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
+
 class NewVisitorTest(StaticLiveServerTestCase):
+	
+    @classmethod
+    def setUpClass(cls):
+        for arg in sys.argv:
+            if 'liveserver' in arg:
+                cls.server_url = 'http://' + arg.split('=')[1]
+                return
+        super().setUpClass()
+        cls.server_url = cls.live_server_url
+	
+    @classmethod
+    def tearDownClass(cls):
+        if cls.server_url == cls.live_server_url:
+            super().tearDownClass()
+
     def setUp(self):
         self.browser = webdriver.Firefox()
         self.browser.implicitly_wait(3)
@@ -17,7 +34,7 @@ class NewVisitorTest(StaticLiveServerTestCase):
 
     def test_layout_and_style(self):
         # Rob goes to the home page
-        self.browser.get(self.live_server_url)
+        self.browser.get(self.server_url)
         self.browser.set_window_size(1024, 768)
 
         # Rob notices the input box is nicely centered.
@@ -41,7 +58,7 @@ class NewVisitorTest(StaticLiveServerTestCase):
     def test_can_start_a_list_and_retrieve_it_later(self):
         # Rob heard about a new online to-do list app. He wants to see if it might be useful for the IT
         # department at ABI Research.
-        self.browser.get(self.live_server_url)
+        self.browser.get(self.server_url)
         self.browser.set_window_size(1024,768)
 
         # He notices the page title and header mention to-do lists.
@@ -84,7 +101,7 @@ class NewVisitorTest(StaticLiveServerTestCase):
         self.browser = webdriver.Firefox()
 
 # Peter visits the home page. There is no sign of Rob's list.
-        self.browser.get(self.live_server_url)
+        self.browser.get(self.server_url)
         page_text = self.browser.find_element_by_tag_name('body').text
         self.assertNotIn('Move new online UPS', page_text)
         self.assertNotIn('Setup the UPS', page_text)
