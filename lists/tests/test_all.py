79344f2fd91be9ad64ea30ce2ac57d1074097895
tfarrell-vz <farrell@abiresearch.com>
2014-10-14 12:32:59 -0400

    Moved unit tests to their own folder in a single file. Relative import had to be changed since they are now in a tests subdirectory.

--- lists/tests/test_all.py
+++ lists/tests/test_all.py
@@ -3,8 +3,8 @@ from django.test import TestCase
 from django.http import HttpRequest
 from django.template.loader import render_to_string
 
-from .models import Item, List
-from .views import home_page
+from lists.models import Item, List
+from lists.views import home_page
 
 
 class HomePageTest(TestCase):
