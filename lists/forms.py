c4efffd8935e7b97fca1c01db54544006bd9edb3
tfarrell-vz <farrell@abiresearch.com>
2014-10-15 17:21:22 -0400

    Forms, forms, FORMS

--- lists/forms.py
+++ lists/forms.py
@@ -1,7 +1,9 @@
 from django import forms
+from django.core.exceptions import ValidationError
 
 from .models import Item
 
+DUPLICATE_ITEM_ERROR = "You can't have two identical items"
 EMPTY_LIST_ERROR = "You can't have an empty list item"
 class ItemForm(forms.models.ModelForm):
 
@@ -22,5 +24,15 @@ class ItemForm(forms.models.ModelForm):
         self.instance.list = for_list
         return super().save()
 
+class ExistingListItemForm(ItemForm):
+    def __init__(self, for_list, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+    def validate_unique(self):
+        try:
+            self.instance.validate_unique()
+        except ValidationError as e:
+            e.error_dict = {'text': [DUPLICATE_ITEM_ERROR]}
+            self._update_errors(e)
 
 
