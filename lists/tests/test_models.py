30e97983305586e95c6fdafa0f114f887c1e8c7d
tfarrell-vz <farrell@abiresearch.com>
2014-10-15 16:50:03 -0400

    Implement duplicate item validation at model layer.

--- lists/tests/test_models.py
+++ lists/tests/test_models.py
@@ -9,13 +9,13 @@ class ListAndItemModelTest(TestCase):
         list_.save()
 
         first_item = Item()
-        first_item.text = 'The first (ever) list item'
         first_item.list = list_
+        first_item.text = 'The first (ever) list item'
         first_item.save()
 
         second_item = Item()
-        second_item.text = 'Item the second'
         second_item.list = list_
+        second_item.text = 'Item the second'
         second_item.save()
 
         saved_list = List.objects.first()
@@ -42,4 +42,32 @@ class ListAndItemModelTest(TestCase):
         list_ = List.objects.create()
         self.assertEqual(list_.get_absolute_url(), '/lists/%d/' % (list_.id,))
 
+    def test_duplicate_items_are_invalid(self):
+        list_ = List.objects.create()
+        Item.objects.create(list=list_, text='bla')
+        with self.assertRaises(ValidationError):
+            item = Item(list=list_, text='bla')
+            item.full_clean()
+
+
+    def test_CAN_save_same_item_to_different_lists(self):
+        list1 = List.objects.create()
+        list2 = List.objects.create()
+        Item.objects.create(list=list1, text='bla')
+        item = Item(list=list2, text='bla')
+        item.full_clean() # should not raise
+
+    def test_list_ordering(self):
+        list1 = List.objects.create()
+        item1 = Item.objects.create(list=list1, text='i1')
+        item2 = Item.objects.create(list=list1, text='item 2')
+        item3 = Item.objects.create(list=list1, text='3')
+        self.assertEqual(
+            list(Item.objects.all()),
+            [item1, item2, item3]
+        )
+
+    def test_string_representation(self):
+        item = Item(text='some text')
+        self.assertEqual(str(item), 'some text')
 
