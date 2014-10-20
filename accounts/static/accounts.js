8116a1782c0904834546693cc5143d35fe540b1b
tfarrell-vz <farrell@abiresearch.com>
2014-10-20 18:09:44 -0400

    Javascript not quite working through the template.

---
+++ accounts/static/accounts.js
@@ -0,0 +1,14 @@
+/*global $ */
+
+var initialize = function (navigator) {
+    $('#id_login').on('click', function() {
+        navigator.id.request();
+    });
+};
+
+window.Superlists = {
+    Accounts: {
+        initialize: initialize
+    }
+};
+
