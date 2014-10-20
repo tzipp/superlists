2e1d27b47ceac359a2ab96a9767ca6774b30b7a4
tfarrell-vz <farrell@abiresearch.com>
2014-10-20 12:06:31 -0400

    Changed to an event listener pattern

--- lists/static/list.js
+++ lists/static/list.js
@@ -1,14 +1,22 @@
-/*global $, test, equal */
+/*global jQuery */
+jQuery(document).ready(function ($) {
 
-$(document).ready(function () {
-    $('input').on('keypress', function() {
-        $('.has-error').hide();
-    });
-});
+    var bindEventListeners = function () {
+        $('input[name="text"]').on('keypress', function () {
+            $('.has-error').hide();
+        });
 
-$(document).ready(function () {
-    $('input').on('click', function() {
-        $('.has-error').hide();
+        $('input[name="text"]').on('click', function() {
+            $('.has-error').hide();
+        });
+    };
+
+    $.extend(window, true, {
+        'Superlists': {
+            'Lists': {
+                'bindEventListeners': bindEventListeners
+            }
+        }
     });
 });
 
